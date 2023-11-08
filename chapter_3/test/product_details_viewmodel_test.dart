dca2e901ace019f9e5a4bfc92a0e2e20df4d91f9
Dane Mackier <dane.mackier@outlook.com>
2023-11-08 07:02:31 +0200

    Adds the unit test for the new chapter

--- chapter_3/test/product_details_viewmodel_test.dart
+++ chapter_3/test/product_details_viewmodel_test.dart
@@ -1,9 +1,28 @@
+import 'package:chapter_3/product_data.dart';
+import 'package:chapter_3/viewmodels/product_details_viewmodel.dart';
 import 'package:test/test.dart';
+
+ProductDetailsViewModel _getModel() =>
+    ProductDetailsViewModel(product: bbqChickenPizza);
 
 void main() {
   group('ProductDetailsViewmodelTest -', () {
     group('addProduct -', () {
-      test('', () {});
+      test(
+        'When we add a product without the required options selected, should show the user a dialog with the title of the optionGroup',
+        () {
+          final viewModel = _getModel();
+
+          viewModel.addProduct();
+
+          expect(
+            true,
+            false,
+            reason:
+                'We don\'t have a way to confirm the results of this test without mocking',
+          );
+        },
+      );
     });
 
     group('checkProductAvailability -', () {
